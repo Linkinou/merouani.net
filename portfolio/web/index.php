0afce5893d0109cbbb80dab4329fe3693b2a6289
Linkinou <cedric.merouani@gmail.com>
2016-09-02 14:20:28 +0200

    [Portfolio] Handling locale fallback correctly

--- portfolio/web/index.php
+++ portfolio/web/index.php
@@ -4,34 +4,43 @@ require_once __DIR__.'/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Translation\Loader\YamlFileLoader;
 use Symfony\Component\Translation\Translator;
+use Silex\Provider\LocaleServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\TranslationServiceProvider;
+
+const DEFAULT_LOCALE = 'en';
 
 $app = new Silex\Application();
-
 /** Libs registration */
-$app->register(new Silex\Provider\TwigServiceProvider(), [
+$app->register(new LocaleServiceProvider());
+$app->register(new TwigServiceProvider(), [
     'twig.path' => __DIR__.'/views',
 ]);
-$app->register(new Silex\Provider\LocaleServiceProvider());
-$app->register(new Silex\Provider\TranslationServiceProvider(), [
-    'locale_fallbacks' => ['en'],
+$app->register(new TranslationServiceProvider(), [
+    'locale_fallbacks' => [DEFAULT_LOCALE],
 ]);
 
 $app->extend('translator', function(Translator $translator, $app) {
     $translator->addLoader('yaml', new YamlFileLoader());
 
-    $translator->addResource('yaml', __DIR__.'/locales/en.yml', 'en');
     $translator->addResource('yaml', __DIR__.'/locales/fr.yml', 'fr');
+    $translator->addResource('yaml', __DIR__.'/locales/en.yml', 'en');
 
     return $translator;
 });
 
 /** Definitions */
 $app['debug'] = true;
-/** Routes */
-$app->get('/{_locale}/', function (Request $request) use ($app) {
+$app['locale'] = DEFAULT_LOCALE;
+
+$mainRoute = function (Request $request) use ($app) {
     $app['asset_path'] = $request->getBasePath() . '/assets/';
 
     return $app['twig']->render('main/index.twig', []);
-});
+};
+
+/** Routes */
+$app->get('/', $mainRoute);
+$app->get('/{_locale}/', $mainRoute);
 
 $app->run();
