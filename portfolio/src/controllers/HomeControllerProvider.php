499951c07558415c9c98b16a5ede149f359f6867
Linkinou <cedric.merouani@gmail.com>
2016-09-25 13:04:32 +0200

    [Portfolio] Adding alfred project content

--- portfolio/src/controllers/HomeControllerProvider.php
+++ portfolio/src/controllers/HomeControllerProvider.php
@@ -10,6 +10,7 @@ class HomeControllerProvider implements ControllerProviderInterface
 {
     const   ROUTE_HOME = 'home';
     const   ROUTE_PORTFOLIO = 'portfolio';
+    const   ROUTE_ALFRED = 'alfred';
 
     public function connect(Application $app)
     {
@@ -23,6 +24,10 @@ class HomeControllerProvider implements ControllerProviderInterface
         $controllers->get('/{_locale}/portfolio', 'Portfolio\HomeControllerProvider::portfolioProject')
                     ->bind(self::ROUTE_PORTFOLIO);
 
+        $controllers->get('/alfred', 'Portfolio\HomeControllerProvider::alfredProject');
+        $controllers->get('/{_locale}/alfred', 'Portfolio\HomeControllerProvider::alfredProject')
+            ->bind(self::ROUTE_ALFRED);
+
         return $controllers;
     }
 
@@ -31,7 +36,7 @@ class HomeControllerProvider implements ControllerProviderInterface
         $app['asset_path'] = $request->getBaseUrl() . '/';
         $app['current_route'] = self::ROUTE_HOME;
 
-        return $app['twig']->render('main/index.twig', []);
+        return $app['twig']->render('main/index.html.twig', []);
     }
 
     public function portfolioProject(Application $app, Request $request)
@@ -40,5 +45,13 @@ class HomeControllerProvider implements ControllerProviderInterface
         $app['current_route'] = self::ROUTE_PORTFOLIO;
 
         return $app['twig']->render('main/portfolio.html.twig', []);
+    }
+
+    public function alfredProject(Application $app, Request $request)
+    {
+        $app['asset_path'] = $request->getBaseUrl() . '/';
+        $app['current_route'] = self::ROUTE_ALFRED;
+
+        return $app['twig']->render('main/alfred.html.twig', []);
     }
 }
