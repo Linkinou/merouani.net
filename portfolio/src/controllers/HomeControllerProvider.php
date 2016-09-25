f5b99167061881b7ff7f9f9f26885bac0ee045f9
Linkinou <cedric.merouani@gmail.com>
2016-09-25 12:22:26 +0200

    [Portfolio] Adding portfolio project content

--- portfolio/src/controllers/HomeControllerProvider.php
+++ portfolio/src/controllers/HomeControllerProvider.php
@@ -8,17 +8,20 @@ use Silex\Application;
 
 class HomeControllerProvider implements ControllerProviderInterface
 {
+    const   ROUTE_HOME = 'home';
+    const   ROUTE_PORTFOLIO = 'portfolio';
+
     public function connect(Application $app)
     {
         $controllers = $app['controllers_factory'];
 
         $controllers->get('/', 'Portfolio\HomeControllerProvider::home');
         $controllers->get('/{_locale}/', 'Portfolio\HomeControllerProvider::home')
-                    ->bind('home');
+                    ->bind(self::ROUTE_HOME);
 
         $controllers->get('/portfolio', 'Portfolio\HomeControllerProvider::portfolioProject');
         $controllers->get('/{_locale}/portfolio', 'Portfolio\HomeControllerProvider::portfolioProject')
-                    ->bind('portfolio');
+                    ->bind(self::ROUTE_PORTFOLIO);
 
         return $controllers;
     }
@@ -26,6 +29,7 @@ class HomeControllerProvider implements ControllerProviderInterface
     public function home(Application $app, Request $request)
     {
         $app['asset_path'] = $request->getBaseUrl() . '/';
+        $app['current_route'] = self::ROUTE_HOME;
 
         return $app['twig']->render('main/index.twig', []);
     }
@@ -33,6 +37,7 @@ class HomeControllerProvider implements ControllerProviderInterface
     public function portfolioProject(Application $app, Request $request)
     {
         $app['asset_path'] = $request->getBaseUrl() . '/';
+        $app['current_route'] = self::ROUTE_PORTFOLIO;
 
         return $app['twig']->render('main/portfolio.html.twig', []);
     }
