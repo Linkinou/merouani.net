9a0e967a6b7a1f72d051533fccb7f69aef1d6703
Merouani Cédric <cedric.merouani@gmail.com>
2016-09-25 19:56:42 +0200

    [Portfolio] Adding some content

--- portfolio/src/controllers/HomeControllerProvider.php
+++ portfolio/src/controllers/HomeControllerProvider.php
@@ -8,12 +8,35 @@ use Silex\Application;
 
 class HomeControllerProvider implements ControllerProviderInterface
 {
+    const   ROUTE_HOME = 'home';
+    const   ROUTE_PORTFOLIO = 'portfolio';
+    const   ROUTE_ALFRED = 'alfred';
+    const   ROUTE_SOFTDEV_WEB = 'softdevWeb';
+    const   ROUTE_CONTACT = 'contact';
+
     public function connect(Application $app)
     {
         $controllers = $app['controllers_factory'];
 
         $controllers->get('/', 'Portfolio\HomeControllerProvider::home');
-        $controllers->get('/{_locale}/', 'Portfolio\HomeControllerProvider::home');
+        $controllers->get('/{_locale}/', 'Portfolio\HomeControllerProvider::home')
+                    ->bind(self::ROUTE_HOME);
+
+        $controllers->get('/portfolio', 'Portfolio\HomeControllerProvider::portfolioProject');
+        $controllers->get('/{_locale}/portfolio', 'Portfolio\HomeControllerProvider::portfolioProject')
+                    ->bind(self::ROUTE_PORTFOLIO);
+
+        $controllers->get('/alfred', 'Portfolio\HomeControllerProvider::alfredProject');
+        $controllers->get('/{_locale}/alfred', 'Portfolio\HomeControllerProvider::alfredProject')
+                    ->bind(self::ROUTE_ALFRED);
+
+        $controllers->get('/softdev-web', 'Portfolio\HomeControllerProvider::softdevWebProject');
+        $controllers->get('/{_locale}/softdev-web', 'Portfolio\HomeControllerProvider::softdevWebProject')
+                    ->bind(self::ROUTE_SOFTDEV_WEB);
+
+//        $controllers->get('/contact', 'Portfolio\HomeControllerProvider::contactProject');
+//        $controllers->get('/{_locale}/contact', 'Portfolio\HomeControllerProvider::contactProject')
+//                    ->bind(self::ROUTE_CONTACT);
 
         return $controllers;
     }
@@ -21,8 +44,41 @@ class HomeControllerProvider implements ControllerProviderInterface
     public function home(Application $app, Request $request)
     {
         $app['asset_path'] = $request->getBaseUrl() . '/';
+        $app['current_route'] = self::ROUTE_HOME;
 
-        return $app['twig']->render('main/index.twig', []);
+        return $app['twig']->render('main/index.html.twig', []);
     }
+
+    public function portfolioProject(Application $app, Request $request)
+    {
+        $app['asset_path'] = $request->getBaseUrl() . '/';
+        $app['current_route'] = self::ROUTE_PORTFOLIO;
+
+        return $app['twig']->render('main/projects/portfolio.html.twig', []);
+    }
+
+    public function alfredProject(Application $app, Request $request)
+    {
+        $app['asset_path'] = $request->getBaseUrl() . '/';
+        $app['current_route'] = self::ROUTE_ALFRED;
+
+        return $app['twig']->render('main/projects/alfred.html.twig', []);
+    }
+
+    public function softdevWebProject(Application $app, Request $request)
+    {
+        $app['asset_path'] = $request->getBaseUrl() . '/';
+        $app['current_route'] = self::ROUTE_SOFTDEV_WEB;
+
+        return $app['twig']->render('main/projects/softdev-web.html.twig', []);
+    }
+
+//    public function contactProject(Application $app, Request $request)
+//    {
+//        $app['asset_path'] = $request->getBaseUrl() . '/';
+//        $app['current_route'] = self::ROUTE_CONTACT;
+//
+//        return $app['twig']->render('main/contact.html.twig', []);
+//    }
 
 }
