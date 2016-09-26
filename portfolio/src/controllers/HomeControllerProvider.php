181b388b3609d6f9b9209b77d0e74b829aebaebe
Linkinou <cedric.merouani@gmail.com>
2016-09-26 23:10:21 +0200

    [Portfolio] Wildcat Attack project content

--- portfolio/src/controllers/HomeControllerProvider.php
+++ portfolio/src/controllers/HomeControllerProvider.php
@@ -12,6 +12,7 @@ class HomeControllerProvider implements ControllerProviderInterface
     const   ROUTE_PORTFOLIO = 'portfolio';
     const   ROUTE_ALFRED = 'alfred';
     const   ROUTE_SOFTDEV_WEB = 'softdevWeb';
+    const   ROUTE_WILDCAT = 'wildcat';
     const   ROUTE_CONTACT = 'contact';
 
     public function connect(Application $app)
@@ -33,6 +34,10 @@ class HomeControllerProvider implements ControllerProviderInterface
         $controllers->get('/softdev-web', 'Portfolio\HomeControllerProvider::softdevWebProject');
         $controllers->get('/{_locale}/softdev-web', 'Portfolio\HomeControllerProvider::softdevWebProject')
                     ->bind(self::ROUTE_SOFTDEV_WEB);
+
+        $controllers->get('/wildcat-attack', 'Portfolio\HomeControllerProvider::wildcatProject');
+        $controllers->get('/{_locale}/wildcat-attack', 'Portfolio\HomeControllerProvider::wildcatProject')
+            ->bind(self::ROUTE_WILDCAT);
 
 //        $controllers->get('/contact', 'Portfolio\HomeControllerProvider::contactProject');
 //        $controllers->get('/{_locale}/contact', 'Portfolio\HomeControllerProvider::contactProject')
@@ -71,6 +76,14 @@ class HomeControllerProvider implements ControllerProviderInterface
         $app['current_route'] = self::ROUTE_SOFTDEV_WEB;
 
         return $app['twig']->render('main/projects/softdev-web.html.twig', []);
+    }
+
+    public function wildcatProject(Application $app, Request $request)
+    {
+        $app['asset_path'] = $request->getBaseUrl() . '/';
+        $app['current_route'] = self::ROUTE_WILDCAT;
+
+        return $app['twig']->render('main/projects/wildcat.html.twig', []);
     }
 
 //    public function contactProject(Application $app, Request $request)
