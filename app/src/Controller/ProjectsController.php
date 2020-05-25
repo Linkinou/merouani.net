ca9baff496a458a1b7bf9c9fed1dda21b7ebc833
Linkinou <cedric.merouani@gmail.com>
2020-05-25 15:16:02 +0200

    Adding ouimiam and bdg projects

--- app/src/Controller/ProjectsController.php
+++ app/src/Controller/ProjectsController.php
@@ -3,15 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProjectsController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
      * @Route("/{_locale}", name="homepage")
      */
-    public function home()
+    public function index(string $_locale = 'en') : Response
     {
         return $this->render('main/index.html.twig');
     }
@@ -19,7 +19,7 @@ class ProjectsController extends AbstractController
     /**
      * @Route("/{_locale}/portfolio", name="portfolio")
      */
-    public function portfolio()
+    public function portfolio() : Response
     {
         return $this->render('main/projects/portfolio.html.twig');
     }
@@ -27,7 +27,7 @@ class ProjectsController extends AbstractController
     /**
      * @Route("/{_locale}/alfred", name="alfred")
      */
-    public function alfred()
+    public function alfred() : Response
     {
         return $this->render('main/projects/alfred.html.twig');
     }
@@ -35,7 +35,7 @@ class ProjectsController extends AbstractController
     /**
      * @Route("/{_locale}/wildcat", name="wildcat")
      */
-    public function wildcat()
+    public function wildcat() : Response
     {
         return $this->render('main/projects/wildcat.html.twig');
     }
@@ -43,8 +43,24 @@ class ProjectsController extends AbstractController
     /**
      * @Route("/{_locale}/softdev-web", name="softdevWeb")
      */
-    public function softdev()
+    public function softdev() : Response
     {
         return $this->render('main/projects/softdev-web.html.twig');
+    }
+
+    /**
+     * @Route("/{_locale}/ouimiam", name="ouimiam")
+     */
+    public function ouimiam() : Response
+    {
+        return $this->render('main/projects/ouimiam.html.twig');
+    }
+
+    /**
+     * @Route("/{_locale}/bdg", name="bdg")
+     */
+    public function bdg() : Response
+    {
+        return $this->render('main/projects/bdg.html.twig');
     }
 }
